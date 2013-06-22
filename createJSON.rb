7a54ad4d702181452e87d1eade92b3ff2dcb5030
Adit Kumar <akumar@palantir.com>
2013-06-23 00:36:12 +0100

    added data

---
+++ createJSON.rb
@@ -0,0 +1,16 @@
+require 'csv'
+
+output = "{ \"type\": \"FeatureCollection\",
+  \"features\": [
+    { \"type\": \"Feature\",
+      \"geometry\": {\"type\": \"Point\", \"coordinates\": [102.0, 0.5]},
+      \"properties\": {\"prop0\": \"value0\"}
+      },
+     ]
+   }"
+
+CSV.foreach('stationentrances.csv', :headers => true) do |entrance| 
+	puts entrance[0]
+end
+
+File.open('./mta-entrances.geojson','w') {|f| f.write(output)}
