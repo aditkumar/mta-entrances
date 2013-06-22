34fff60872db9d325d4556f284fb7176deb75574
Adit Kumar <akumar@palantir.com>
2013-06-23 00:55:15 +0100

    added data

--- createJSON.rb
+++ createJSON.rb
@@ -6,13 +6,21 @@ output =
 
 c = CSV.table('stationentrances.csv') 
 
-for i in 0..c.size-2
+for i in 0..c.size-1
 	station = "{ \"type\": \"Feature\",
       \"geometry\": {\"type\": \"Point\", \"coordinates\": [#{c[i][28]}, #{c[i][29]}]},
       \"properties\": {\"Name\": \"#{c[i][2]}\"}
       },\n\t\t"
     output << station
 end
+ind = c.size-1
+
+station = "{ \"type\": \"Feature\",
+      \"geometry\": {\"type\": \"Point\", \"coordinates\": [#{c[ind][28]}, #{c[ind][29]}]},
+      \"properties\": {\"Name\": \"#{c[ind][2]}\"}
+      }"
+
+
 
 output << 	"\n\t]
 }"
