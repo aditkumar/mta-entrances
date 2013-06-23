369920cef135f4be49af61a149a7d6aa147d18a4
Adit Kumar <akumar@palantir.com>
2013-06-23 01:46:16 +0100

    styling

--- createJSON.rb
+++ createJSON.rb
@@ -1,5 +1,27 @@
 require 'csv'
 
+def colors(line) 
+	output = "#{}FFFFFF"
+	if ["A","C","E"].include? line 
+		output = #2850AD"
+	elsif ["B","D","F","M"].include? line
+		output = "#FF6319"
+	elsif "G" == line
+		output = "#6CBE45"
+	elsif ["J","Z"].include? line
+		output = "#996633"
+end
+
+def createStation
+end
+
+def createEntrance(entrance,last=false)
+	station = "{ \"type\": \"Feature\",
+      \"geometry\": {\"type\": \"LineString\", \"coordinates\": [[#{entrance[i][4]},#{entrance[i][3]}],[#{entrance[i][29]}, #{entrance[i][28]}]]},
+      \"properties\": {\"Name\": \"#{entrance[i][2]}\" , \"marker-size\" : \"small\"}
+      },\n\t\t"
+end
+
 output = 
 "{ \"type\": \"FeatureCollection\",
   \"features\": ["
@@ -21,11 +43,7 @@ c = CSV.table('stationentrances.csv')
 #       }"
 
 for i in 0..c.size-1
-	station = "{ \"type\": \"Feature\",
-      \"geometry\": {\"type\": \"LineString\", \"coordinates\": [[#{c[i][4]},#{c[i][3]}],[#{c[i][29]}, #{c[i][28]}]]},
-      \"properties\": {\"Name\": \"#{c[i][2]}\" , \"marker-size\" : \"small\"}
-      },\n\t\t"
-    output << station
+    output << createEntrance(c[i])
 end
 ind = c.size-1
 
