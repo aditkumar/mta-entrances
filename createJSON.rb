67c2076eeaa723b668068839bb72d30d135c8d59
Adit Kumar <akumar@palantir.com>
2013-06-23 01:25:09 +0100

    styling

--- createJSON.rb
+++ createJSON.rb
@@ -9,7 +9,7 @@ c = CSV.table('stationentrances.csv')
 for i in 0..c.size-1
 	station = "{ \"type\": \"Feature\",
       \"geometry\": {\"type\": \"Point\", \"coordinates\": [#{c[i][29]}, #{c[i][28]}]},
-      \"properties\": {\"Name\": \"#{c[i][2]}\" , \"marker-size\" : \"small\"
+      \"properties\": {\"Name\": \"#{c[i][2]}\" , \"marker-size\" : \"small\"}
       },\n\t\t"
     output << station
 end
