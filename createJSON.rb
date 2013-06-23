7478bf069d6471256616e69f19e0ca9c2cc18fd8
Adit Kumar <akumar@palantir.com>
2013-06-23 01:29:03 +0100

    styling

--- createJSON.rb
+++ createJSON.rb
@@ -6,9 +6,23 @@ output =
 
 c = CSV.table('stationentrances.csv') 
 
+# for i in 0..c.size-1
+# 	station = "{ \"type\": \"Feature\",
+#       \"geometry\": {\"type\": \"Point\", \"coordinates\": [#{c[i][29]}, #{c[i][28]}]},
+#       \"properties\": {\"Name\": \"#{c[i][2]}\" , \"marker-size\" : \"small\"}
+#       },\n\t\t"
+#     output << station
+# end
+# ind = c.size-1
+
+# station = "{ \"type\": \"Feature\",
+#       \"geometry\": {\"type\": \"Point\", \"coordinates\": [#{c[ind][29]}, #{c[ind][28]}]},
+#       \"properties\": {\"Name\": \"#{c[ind][2]}\" , \"marker-size\" : \"small\"}
+#       }"
+
 for i in 0..c.size-1
 	station = "{ \"type\": \"Feature\",
-      \"geometry\": {\"type\": \"Point\", \"coordinates\": [#{c[i][29]}, #{c[i][28]}]},
+      \"geometry\": {\"type\": \"LineString\", \"coordinates\": [[#{c[i][4]},#{c[i][3]}],[#{c[i][29]}, #{c[i][28]}]]},
       \"properties\": {\"Name\": \"#{c[i][2]}\" , \"marker-size\" : \"small\"}
       },\n\t\t"
     output << station
@@ -16,7 +30,7 @@ end
 ind = c.size-1
 
 station = "{ \"type\": \"Feature\",
-      \"geometry\": {\"type\": \"Point\", \"coordinates\": [#{c[ind][29]}, #{c[ind][28]}]},
+      \"geometry\": {\"type\": \"LineString\", \"coordinates\": [[#{c[ind][4]},#{c[ind][3]}],[#{c[ind][29]}, #{c[ind][28]}]]},
       \"properties\": {\"Name\": \"#{c[ind][2]}\" , \"marker-size\" : \"small\"}
       }"
 
@@ -27,4 +41,4 @@ output << "\n\t]
 }"
 
 
-File.open('./mta-entrances.geojson','w') {|f| f.write(output)}
+File.open('./mta-entrances-lines.geojson','w') {|f| f.write(output)}
