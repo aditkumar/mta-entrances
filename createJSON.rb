4b4c3a3c0beff9728002039c6aa7cbe723aeec26
Adit Kumar <akumar@palantir.com>
2013-06-23 02:06:12 +0100

    styling

--- createJSON.rb
+++ createJSON.rb
@@ -66,6 +66,7 @@ for i in 0..c.size-1
     output << createEntrance(c[i])
     if !allStations.include? c[i][2]
     	output << createStation(c[i])
+    	allStations.push(c[i][2])
     end
 end
 output << createEntrance(c[c.size-1],true)
